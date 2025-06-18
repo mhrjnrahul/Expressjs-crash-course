30e9b851944158e15392c94ad999941346e2580a
mhrjnrahul <maharjanrahul58@gmail.com>
2025-06-18 15:48:47 +0545

    added static folder for routes

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello');
-});
+//setup static folder
+app.use(express.static('public'));
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
