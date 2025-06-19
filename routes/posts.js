af64a2fe4f5ccc024a2dea16bf1627b7832ec5eb
mhrjnrahul <maharjanrahul58@gmail.com>
2025-06-19 15:46:54 +0545

    Routes setup

---
+++ routes/posts.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const router = express.Router();
+
+const posts = [
+  {
+    id: 1,
+    title: "First Post",
+  },
+  {
+    id: 2,
+    title: "Second Post",
+  },
+  {
+    id: 3,
+    title: "Third Post",
+  },
+];
+
+//get all posts
+router.get("/", (req, res) => {
+  //limiter
+  const limit = parseInt(req.query.limit) || posts.length;
+
+  if (!isNaN(limit) && limit > 0) {
+    res.status(200).json(posts.slice(0, limit));
+  } else {
+    res.json(posts);
+  }
+});
+
+//get a single post by id
+router.get("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  res.json(posts.filter((post) => post.id === id));
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,41 +2,16 @@ const express = require("express");
 
 const app = express();
 
-const posts = [
-  {
-    id: 1,
-    title: "First Post",
-  },
-  {
-    id: 2,
-    title: "Second Post",
-  },
-  {
-    id: 3,
-    title: "Third Post",
-  },
-];
+const posts = require("./routes/posts");
+
+
 
 //setup static folder
 // app.use(express.static('public'));
 
-//get all posts
-app.get("/api/posts", (req, res) => {
-  //limiter
-  const limit = parseInt(req.query.limit) || posts.length;
+//Routes
+app.use('/api/posts', posts);
 
-  if (!isNaN(limit) && limit > 0) {
-    res.status(200).json(posts.slice(0, limit));
-  } else {
-    res.json(posts);
-  }
-});
-
-//get a single post by id
-app.get("/api/posts/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  res.json(posts.filter((post) => post.id === id));
-});
 
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
