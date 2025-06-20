b4c30c8429fefc304babff1b5b301d90e4aa6994
mhrjnrahul <maharjanrahul58@gmail.com>
2025-06-20 12:05:46 +0545

    CRUD operations

--- routes/posts.js
+++ routes/posts.js
@@ -34,4 +34,46 @@ router.get("/:id", (req, res) => {
   res.json(posts.filter((post) => post.id === id));
 });
 
+//create a new post
+router.post("/", (req, res) => {
+  const newPost = {
+    id: posts.length + 1,
+    title: req.body.title,
+  };
+
+  if (!newPost.title) {
+    return res.status(400).json({ error: "Title is required" });
+  }
+
+  posts.push(newPost);
+  res.status(201).json(newPost);
+});
+
+//update post by id
+router.put("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+
+  if (!post) {
+  return res.status(404).json({ error: "Post not found" });
+}
+
+post.title = req.body.title || post.title;
+res.status(200).json(post);
+});
+
+//delete post by id
+router.delete("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = posts.findIndex((post) => post.id === id);
+
+  if (index === -1) {
+    return res.status(404).json({ error: "Post not found" });
+  }
+
+  posts.splice(index, 1);
+  res.status(204).send();
+});
+
+
 export default router;
